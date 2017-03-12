57689a232d108927749b3dde7bb49a78500bb20e
Harikrishnan R <harikrishnanr994@gmail.com>
2017-03-12 16:31:43 +0530

    edited register

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 <?php require 'inc/views/template_head_end.php'; ?>
 
 <!-- Login Content -->
-<div class="bg-white pulldown">
+<div class="bg-white">
     <div class="content content-boxed overflow-hidden">
         <div class="row">
             <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
@@ -18,7 +18,7 @@
                     <!-- Login Form -->
                     <!-- jQuery Validation (.js-validation-login class is initialized in js/pages/base_pages_login.js) -->
                     <!-- For more examples you can check out https://github.com/jzaefferer/jquery-validation -->
-                    <form class="js-validation-login form-horizontal push-30-t" action="action_register.php" method="post">
+                    <form class="js-validation-login form-horizontal push-30-t" action="actionregister.php" method="post">
                         <div class="form-group">
                             <div class="col-xs-12">
                                 <div class="form-material form-material-primary floating">
@@ -69,14 +69,16 @@
                 </div>
             </div>
         </div>
+        <div class="text-center animated fadeInUp">
+            <small class="text-muted"><span class="js-year-copy"></span> &copy; <?php echo $one->name . ' ' . $one->version; ?></small>
+        </div>
     </div>
+<br><br>
 </div>
 <!-- END Login Content -->
 
 <!-- Login Footer -->
-<div class="pulldown push-30-t text-center animated fadeInUp">
-    <small class="text-muted"><span class="js-year-copy"></span> &copy; <?php echo $one->name . ' ' . $one->version; ?></small>
-</div>
+
 <!-- END Login Footer -->
 
 <?php require 'inc/views/template_footer_start.php'; ?>
